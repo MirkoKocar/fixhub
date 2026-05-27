c30d4ada1d3f2154883e1856fde9452e6bfabcb8
MirkoKocar <166563796+MirkoKocar@users.noreply.github.com>
2026-05-26 22:54:53 -0300

    Update supabase.js

--- fixhub-v4/src/supabase.js
+++ fixhub-v4/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://tkuwxamldedwnrkqklrl.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdXd4YW1sZGVkd25ya3FrbHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczOTg0NzksImV4cCI6MjA2Mjk3NDQ3OX0.AzMDsBIOWmQRsxlp2y55U6YG8JTnkFLJRvPy48DIYHI'
+const supabaseAnonKey = 'sb_publishable_zcdeU998Z3sy_uqN8MdQaQ_nsMe_J-N'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
