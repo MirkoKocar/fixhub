2f75674af0599bc47a5413c3195d46f6dcf2ab52
MirkoKocar <166563796+MirkoKocar@users.noreply.github.com>
2026-05-27 19:24:55 -0300

    Delete fixhub-v4 directory

--- fixhub-v4/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://tkuwxamldedwnrkqklrl.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdXd4YW1sZGVkd25ya3FrbHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMjU3NTQsImV4cCI6MjA5NDcwMTc1NH0.vZkGPfN4kYKHQ2z1pNnmXDFXJsGr-S1gLgXTRz9D5Mk'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
