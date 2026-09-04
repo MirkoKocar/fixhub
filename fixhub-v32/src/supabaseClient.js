64e0775b208bf5d462918b87fc18f587f240221d
MirkoKocar <166563796+MirkoKocar@users.noreply.github.com>
2026-09-04 09:35:11 -0300

    Update supabaseClient.js

--- fixhub-v32/src/supabaseClient.js
+++ fixhub-v32/src/supabaseClient.js
@@ -4,6 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://qrxkrqfcltdoaqsggwal.supabase.co'
 
 // Encuentras esta clave en Supabase -> Project Settings -> API (anon public key)
-const supabaseAnonKey = 'TU_CLAVE_ANON_PUBLIC_DE_SUPABASE' 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyeGtycWZjbHRkb2Fxc2dnd2FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwNjU3ODAsImV4cCI6MjA5NzY0MTc4MH0.Qv8BqTHctSCvJ-eZkfiE8znU_7GNeqffij0_8u5cgr8' 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
